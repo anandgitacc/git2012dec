d157d3363bf5d43ff8123d0fa9aeaedc6fb0b7a4
anandgitacc <anand.akmail@gmail.com>
2022-12-21 12:37:47 +0530

    day2

---
+++ src/org/client/Client.java
@@ -0,0 +1,15 @@
+package org.client;
+
+public class Client {
+	
+	public void clName() {
+		System.out.println("Client name: CTS");
+	}
+	
+	public void clLocation() {
+		System.out.println("Client location: Chennai");
+	}
+	
+	
+
+}
--- src/org/employee/EmployeeDetails.java
+++ src/org/employee/EmployeeDetails.java
@@ -1,5 +1,8 @@
 package org.employee;
 
+import org.client.Client;
+
+//            class
 public class EmployeeDetails {
 	
 	// private -> Access specifier for given method
@@ -7,23 +10,32 @@ public class EmployeeDetails {
 	// empName -> method name
 	// ()      -> arguments place / input place / parameter place
 	// {}      -> block of the method or body of the method
-	
 	// ""      -> String (accept all data)
+	
+	//           method1
 	private void empName() {
 		System.out.println("Employee name : anand");
 	}
-	
+//  method1
 	private void empMobile() {
 		System.out.println("Employee mobile: 8939278836");
 	}
-
+//                    main method
 	public static void main(String[] args) {
 		
+		Client cl = new Client();
+		cl.clName();
+		cl.clLocation();
+		// object creation
 		// ClassName ref = new ClassName();
-		EmployeeDetails e = new EmployeeDetails();
+		EmployeeDetails emp = new EmployeeDetails();
+		// method call
+		emp.empName();
+		emp.empMobile();
 		
-		e.empName();
-		e.empMobile();
+		Company c = new Company();
+		c.comName();
+		c.comId();
 		
 		
 	}
